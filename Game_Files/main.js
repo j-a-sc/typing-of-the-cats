0fee823a1844390779e0655ff8164e67097132e8
jack-a-scott <jack.scott@ntlworld.com>
2017-09-18 12:06:35 +0100

    styled splash screen and refactored js

--- Game_Files/main.js
+++ Game_Files/main.js
@@ -1,8 +1,9 @@
+// IMPORTANT VARS
 var breakvar = false;
 var $startButton = $(".start-button");
 var $splashScreen = $(".splash-screen");
-// CAT TEMPLATES
 
+// CAT TEMPLATES
 var $topRightCat = $(".top-right-cat");
 var $topLeftCat = $(".top-left-cat");
 var $bottomRightCat = $(".bottom-right-cat");
@@ -13,11 +14,23 @@ var $rightMiddleCat = $(".middle-right-cat");
 var $bottomMiddleCat = $(".bottom-middle-cat");
 var $spawnArray = [$topRightCat, $topLeftCat, $bottomRightCat,
                   $bottomLeftCat, $topMiddleCat, $leftMiddleCat,
-                  $rightMiddleCat, $bottomMiddleCat ]
+                  $rightMiddleCat, $bottomMiddleCat ];
+var catRate = 5000;
 
-var catRate = 500;
+// PHRASE ARRAY
+var phraseArray = ["DOG","MARMOSET","MONKEY", "ELEPHANT",
+				   "BIGGEST CAT","JANE EYRE"];
 
-// THE CATS MUST FLOW
+
+
+
+// GAME STARTING FUNCTION
+
+function startGame (){
+	timer();
+};
+
+// TIMELOOP THAT SPAWNS CATS EVERY catRate
 
 function timer () {
 	setTimeout( function () {
@@ -30,27 +43,55 @@ function timer () {
 	}, catRate)
 };
 
-function startGame (){
-	timer();
-};
+// CLONES TEMPLATE CAT AND PUTS NEW CAT INTO ANIMATION
+
+function spawnCat($template) {
+	var phraseNo = Math.floor(Math.random()*phraseArray.length);
+	$template.clone().appendTo($('body'))
+		.removeClass("invisible").addClass("active-cat")
+		.animate({ left: "45%", top: "40%"}, 
+		         { duration: 6000, complete: youdied });		
+		var $phrase = $template.find(".phrase-holder .cat-phrase");
+		$phrase.html(phraseArray[phraseNo]);
+}
+
+// PERFORMS CAT EXPLOSION ANIMATION AND REMOVES ELEMENT
+
+function catDie(phrase, cat) {
+	phrase.css({
+	  			"visibility": "hidden"
+	  		});
+	 		cat.stop();
+	 		cat.find(".cat-image").css ({
+	 			"background-image": "url('Images/explosion.png')"
+	 		})
+	  		cat.fadeOut(2000, function () {
+	  			cat.remove();
+	  		});
+}
+
+//FUNCTION TO DISPLAY IF CAT REACHES YOU
+
+function youdied() {
+	var body = $('body');
+	body.append('<h1 class="game-over"> YOU DIED </h1>');
+	breakvar = true;
+	$cat = $(".active-cat .cat-phrase").parent().parent();
+	$cat.stop().fadeOut(2000, function () {
+		cat.remove();
+	});
+
+}
 
 $startButton.on("click", function () {
-	$splashScreen.fadeOut(2000, function () {
+	$splashScreen.slideUp(2000, function () {
 		$splashScreen.remove();
 		startGame();
 	})
 });
 
-function spawnCat($template) {
-	$template.clone().appendTo($('body'))
-		.removeClass("invisible")
-		.addClass("active-cat")
-		.animate({ left: "45%", top: "40%"}, 
-		         { duration: 6000, complete: youdied });
-}
 
-// THIS SECTION RECORDS KEYPRESSES AND REMOVES FIRST STRING MEMBER
-// OF THE PHRASE BOX
+// RECORDS KEYPRESSES AND REMOVES FIRST STRING MEMBER OF THE PHRASE BOX
 
 $(document).keydown(function(event) {
 
@@ -68,28 +109,3 @@ $(document).keydown(function(event) {
 	  	}
   	});
 });
-
-function catDie(phrase, cat) {
-	phrase.css({
-	  			"visibility": "hidden"
-	  		});
-	 		cat.stop();
-	 		cat.find(".cat-image").css ({
-	 			"background-image": "url('Images/explosion.png')"
-	 		})
-	  		cat.fadeOut(2000, function () {
-	  			cat.remove();
-	  		});
-}
-
-//FUNCTION TO DISPLAY IF CAT REACHES YOU
-function youdied() {
-	var body = $('body');
-	body.append('<h1 class="game-over"> YOU DIED </h1>');
-	breakvar = true;
-	$cat = $(".active-cat .cat-phrase").parent().parent();
-	$cat.stop().fadeOut(2000, function () {
-		cat.remove();
-	});
-
-}
